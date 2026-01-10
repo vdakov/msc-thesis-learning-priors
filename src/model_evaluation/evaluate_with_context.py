c7af31221982532d9f4f9c9e8cde2d6fa10cd4df
vdakov <v.dakov02@gmail.com>
2026-01-11 00:55:21 +0200

    Better context eval

--- src/model_evaluation/evaluate_with_context.py
+++ src/model_evaluation/evaluate_with_context.py
@@ -3,10 +3,8 @@ import torch
 
 def evaluate_parameter_distributions_on_model(train_X, train_Y, model, device, num_training_points):
     model = model.to(device)
-    train_x = train_X
-    train_y = train_Y
     with torch.no_grad():
-        logits = model((train_x, train_y), context_pos=num_training_points)
+        logits = model((train_X, train_Y), context_pos=num_training_points)
         outputs = torch.exp(torch.log_softmax(logits, -1))
     
     return outputs
