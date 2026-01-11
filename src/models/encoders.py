1e08f5454e5e8bbfb77f7ad0ff7a143474e6b297
vdakov <v.dakov02@gmail.com>
2026-01-11 08:09:40 +0200

    Update encoder impl

--- src/models/encoders.py
+++ src/models/encoders.py
@@ -1,4 +1,16 @@
 import torch.nn as nn
 
+class SimpleMLP(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, output_dim),
+            nn.GELU(),                # This provides the nonlinearity
+            nn.Linear(output_dim, output_dim)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
 LinearEncoder = nn.Linear 
-MLPEncoder = nn.Embedding
+MLPEncoder = SimpleMLP
